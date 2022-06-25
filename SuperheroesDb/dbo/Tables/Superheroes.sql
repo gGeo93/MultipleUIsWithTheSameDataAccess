d1f11b5ad333c50ba961c0dc3ffecb45dbe0bb80
gGeo93 <giorgosfilippas93@gmail.com>
2022-06-25 04:30:47 +0300

    Added the db project to the solution

---
+++ SuperheroesDb/dbo/Tables/Superheroes.sql
@@ -0,0 +1,12 @@
+﻿CREATE TABLE [dbo].[Superheroes] (
+    [Id]              INT            IDENTITY (1, 1) NOT NULL,
+    [HeroName]        NVARCHAR (MAX) NOT NULL,
+    [FirstName]       NVARCHAR (MAX) NOT NULL,
+    [LastName]        NVARCHAR (MAX) NOT NULL,
+    [Location]        NVARCHAR (MAX) NOT NULL,
+    [Rank]            INT            NOT NULL,
+    [NumberOfFriends] INT            NOT NULL,
+    [ImgFileName]     NVARCHAR (MAX) DEFAULT (N'') NOT NULL,
+    CONSTRAINT [PK_Superheroes] PRIMARY KEY CLUSTERED ([Id] ASC)
+);
+
